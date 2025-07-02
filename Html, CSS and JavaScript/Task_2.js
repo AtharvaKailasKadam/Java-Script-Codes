79ce3bc125e1d4e6cad5ff6d82589ada66699f33
Your Name <atharvakailaskadam1809@gmail.com>
2025-07-02 19:10:24 +0530

    Done and Dusted

--- Html, CSS and JavaScript/Task_2.js
+++ Html, CSS and JavaScript/Task_2.js
@@ -7,8 +7,10 @@ divisions[0].innerText = "Hii... Atharva.";
 divisions[1].innerText = "How are You....Atharva";
 divisions[2].innerText = "How is it Going...Atharva";
 
-
+let idx = 0;
 for(div of divisions)
 {
+    div.innerText = `Hello Atharva Kadam ${idx}`;
     console.log(div.innerText);
+    idx++;
 }
