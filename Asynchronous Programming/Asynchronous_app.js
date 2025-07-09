123fc4508283cfa06f1addfe4cb6f94c90bcfa20
Your Name <atharvakailaskadam1809@gmail.com>
2025-07-09 19:13:28 +0530

    Done and Dusted

---
+++ Asynchronous Programming/Asynchronous_app.js
@@ -0,0 +1,5 @@
+// This is the Example of Synchronous Programming in JavaScript
+// i.e. The Code gets Executed in the order it is written.
+console.log("Asynchronous Programming-1");
+console.log("Asynchronous Programming-2");
+console.log("Asynchronous Programming-3");
